958338f45f8c234684853603ea1eafb00f0952a9
Pablo Vinicios <pablo.araujo@alu.ufc.br>
2025-01-15 10:36:48 -0300

    feat: making the source project

---
+++ src/test/java/com/pointdafamilia/pointdafamilia/PointdafamiliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pointdafamilia.pointdafamilia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointdafamiliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
