9296a4292561f0c3880f8358e78d2c40ed6284c1
Pablo Vinicios <pablo.araujo@alu.ufc.br>
2025-01-28 15:30:54 -0300

    feat: validate token configuration

--- src/main/java/com/pointdafamilia/pointdafamilia/services/TokenService.java
+++ src/main/java/com/pointdafamilia/pointdafamilia/services/TokenService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.pointdafamilia.pointdafamilia.entities.User;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -34,6 +35,20 @@ public class TokenService {
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Error while generating token");
         }
+    }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+            .withIssuer("auth-api")
+            .build()
+            .verify(token)
+            .getSubject();
+        }catch(JWTVerificationException exception){
+            throw new RuntimeException("Invalid token");
+        }
+        
     }
 
     private Instant getExpirationDate(){
