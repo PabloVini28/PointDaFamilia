2cd524a9a61e12cfe10c070badfe1145ae4cd8a1
Pablo Vinicios <pablo.araujo@alu.ufc.br>
2025-01-28 15:27:49 -0300

    feat: adding token service to generate it

---
+++ src/main/java/com/pointdafamilia/pointdafamilia/services/TokenService.java
@@ -0,0 +1,43 @@
+package com.pointdafamilia.pointdafamilia.services;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.pointdafamilia.pointdafamilia.entities.User;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+    
+    public String generateToken(User user){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(getExpirationDate())
+                    .sign(algorithm);
+            return token;
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error while generating token");
+        }
+    }
+
+    private Instant getExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
