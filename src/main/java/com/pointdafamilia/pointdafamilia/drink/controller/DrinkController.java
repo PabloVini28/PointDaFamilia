397fb1010122f39e84f1cb8cb1874556545e5587
Pablo Vinas <pablo.araujo@alu.ufc.br>
2025-03-09 15:42:00 -0300

    refactor: delete unsed libs

--- src/main/java/com/pointdafamilia/pointdafamilia/drink/controller/DrinkController.java
+++ src/main/java/com/pointdafamilia/pointdafamilia/drink/controller/DrinkController.java
@@ -7,6 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.pointdafamilia.pointdafamilia.drink.dtos.DrinkDto;
+import com.pointdafamilia.pointdafamilia.drink.dtos.DrinkPatchImage;
+import com.pointdafamilia.pointdafamilia.drink.dtos.DrinkPatchName;
+import com.pointdafamilia.pointdafamilia.drink.dtos.DrinkPatchPrice;
+import com.pointdafamilia.pointdafamilia.drink.dtos.DrinkPatchQuantity;
+import com.pointdafamilia.pointdafamilia.drink.dtos.DrinkPatchType;
+import com.pointdafamilia.pointdafamilia.drink.dtos.DrinkPatchVolume;
 import com.pointdafamilia.pointdafamilia.drink.entity.Drink;
 import com.pointdafamilia.pointdafamilia.drink.service.DrinkService;
 import jakarta.validation.Valid;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
@@ -56,6 +63,42 @@ public class DrinkController {
     public ResponseEntity<Void> deleteDrinkById(Long id) throws Exception{
         drinkService.deleteDrink(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{name}/name")
+    public ResponseEntity<DrinkDto> updateDrinkName(@RequestBody DrinkPatchName drinkPatchName, @PathVariable String name) throws Exception{
+        DrinkDto drink = drinkService.updateName(drinkPatchName, name);
+        return ResponseEntity.ok().body(drink);
+    }
+
+    @PatchMapping("/{name}/volume")
+    public ResponseEntity<DrinkDto> updateDrinkVolume(@RequestBody DrinkPatchVolume drinkPatchVolume, @PathVariable String name) throws Exception{
+        DrinkDto drink = drinkService.updateVolume(drinkPatchVolume, name);
+        return ResponseEntity.ok().body(drink);
+    }
+
+    @PatchMapping("/{name}/type")
+    public ResponseEntity<DrinkDto> updateDrinkType(@RequestBody DrinkPatchType drinkPatchType, @PathVariable String name) throws Exception{
+        DrinkDto drink = drinkService.updateDrinkType(drinkPatchType, name);
+        return ResponseEntity.ok().body(drink);
+    }
+
+    @PatchMapping("/{name}/price")
+    public ResponseEntity<DrinkDto> updateDrinkPrice(@RequestBody DrinkPatchPrice drinkPatchPrice, @PathVariable String name) throws Exception{
+        DrinkDto drink = drinkService.updatePrice(drinkPatchPrice, name);
+        return ResponseEntity.ok().body(drink);
+    }
+
+    @PatchMapping("/{name}/quantity")
+    public ResponseEntity<DrinkDto> updateDrinkQuantity(@RequestBody DrinkPatchQuantity drinkPatchQuantity, @PathVariable String name) throws Exception{
+        DrinkDto drink = drinkService.updateQuantity(drinkPatchQuantity, name);
+        return ResponseEntity.ok().body(drink);
+    }
+
+    @PatchMapping("/{name}/image")
+    public ResponseEntity<DrinkDto> updateDrinkImage(@RequestBody DrinkPatchImage drinkPatchImage, @PathVariable String name) throws Exception{
+        DrinkDto drink = drinkService.updateUrlImage(drinkPatchImage, name);
+        return ResponseEntity.ok().body(drink);
     }
     
 }
