418371c93857d900f26c78c342e3c3fb0ad6226f
Pablo Vinas <pablo.araujo@alu.ufc.br>
2025-03-10 14:55:54 -0300

    feat: creating the SecurityFilter and adding the addFilterBefore at SecurityConfigurations

--- src/main/java/com/pointdafamilia/pointdafamilia/auth/infra/SecurityConfigurations.java
+++ src/main/java/com/pointdafamilia/pointdafamilia/auth/infra/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package com.pointdafamilia.pointdafamilia.auth.infra;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +12,16 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.pointdafamilia.pointdafamilia.auth.filter.SecurityFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+
+    @Autowired
+    private SecurityFilter securityFilter;
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
@@ -37,6 +44,7 @@ public class SecurityConfigurations {
         .requestMatchers(HttpMethod.PATCH, "/api/food/{name}/type").hasRole("ADMIN")
         .anyRequest().authenticated()
         )
+        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
     }
 
