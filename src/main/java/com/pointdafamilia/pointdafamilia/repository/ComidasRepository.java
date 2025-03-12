ba684e878f3e680324c866a60d3bcad16dadc1d4
Pablo Vinícios <128994831+PabloVini28@users.noreply.github.com>
2025-03-12 14:07:34 -0300

    Delete src/main/java/com/pointdafamilia/pointdafamilia/repository directory

--- src/main/java/com/pointdafamilia/pointdafamilia/repository/ComidasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pointdafamilia.pointdafamilia.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pointdafamilia.pointdafamilia.entities.Comida;
-
-
-public interface ComidasRepository  extends JpaRepository<Comida, Long> {
-    Boolean existsByNome(String ComidaDtoNome);
-}
