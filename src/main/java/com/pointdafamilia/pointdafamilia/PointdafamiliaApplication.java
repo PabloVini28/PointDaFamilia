c5dadabdc29f3a2fc41726875e53174edf307c2f
Pablo Vinas <pablo.araujo@alu.ufc.br>
2025-03-12 16:20:31 -0300

    feat: uping the class where it will be reference

--- src/main/java/com/pointdafamilia/pointdafamilia/PointdafamiliaApplication.java
+++ src/main/java/com/pointdafamilia/pointdafamilia/PointdafamiliaApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.pointdafamilia.pointdafamilia.configurations.FileStorageConfig;
+
 @SpringBootApplication
-@EnableConfigurationProperties
+@EnableConfigurationProperties({
+	FileStorageConfig.class
+})
 public class PointdafamiliaApplication {
 
 	public static void main(String[] args) {
